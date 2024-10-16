40c5d8ebff489c21b4a6f29552e70549fe1d64da
Alina Luchytska <aluchitska@gmail.com>
2024-10-16 11:17:31 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,44 +6,35 @@ const listEmployee = document.querySelectorAll('li');
 function parseSalary(salaryString) {
   const cleanedString = salaryString.replace(/[^0-9.-]+/g, '');
 
-  // Перевіряємо, чи це дійсно валідне число
-  if (!isNaN(cleanedString) && cleanedString.trim() !== '') {
-    return parseFloat(cleanedString);
-  } else {
-    return null;
-  }
+  return parseFloat(cleanedString);
 }
 
 function sortList(list) {
   return [...list].sort((a, b) => {
-    // Отримуємо зарплату з data-атрибутів і перетворюємо в число
     const salaryA = parseSalary(a.dataset.salary);
     const salaryB = parseSalary(b.dataset.salary);
 
-    // Сортуємо за спаданням зарплати
     return salaryB - salaryA;
   });
 }
 
 function getEmployees(list) {
   return list.map((item) => {
-    // Використовуємо деструктуризацію для dataset
     const { position, salary, age } = item.dataset;
-    const nameObj = item.textContent.trim(); // Отримуємо ім'я
+    const nameObj = item.textContent.trim();
 
     return {
       nameObj,
-      position, // Використовуємо деструктуризоване значення
-      salary: parseSalary(salary), // Конвертуємо зарплату в число
-      age: parseInt(age), // Конвертуємо вік в число
+      position,
+      salary: parseSalary(salary),
+      age: parseInt(age),
     };
   });
 }
 
-function getSortedEmployees(list) {
-  const resSort = sortList(list);
+const sortedList = sortList(listEmployee);
+const ul = document.querySelector('ul');
 
-  return getEmployees(resSort);
-}
-
-getSortedEmployees(listEmployee);
+ul.innerHTML = '';
+sortedList.forEach((item) => ul.appendChild(item));
+getEmployees(sortedList);
